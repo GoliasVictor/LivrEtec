e55af12aad36c97c167e87c85b6cbe4c67e44de5
GoliasVictor <Dev.jvsb@gmail.com>
2022-12-03 00:53:57 -0300

    Criacao LivrEtec.Interno.Cliente

---
+++ src/LivrEtec.Interno.Cliente/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LivrEtec.Interno.Cliente
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/LivrEtec.Interno.Cliente/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LivrEtec.Interno.Cliente
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
