772779af09f3ad4cfcb48f12a09a9a9a56bbe113
GoliasVictor <dev.jvsb@gmail.com>
2022-12-17 21:57:35 -0300

    Conserto do merge

--- src/LivrEtec.Testes/RepLivroRPC.cs
+++ src/LivrEtec.Testes/RepLivroRPC.cs
@@ -15,38 +15,41 @@ namespace LivrEtec.Testes
             _logger = logger;
         }
 
-        public void Editar(Livro livro)
+        public async Task EditarAsync(Livro livro)
         {
             _ = livro ?? throw new ArgumentNullException(nameof(livro));
             livro.Tags ??= new List<Tag>();
-            LivrosClientRPC.Editar(livro);
+            await LivrosClientRPC.EditarAsync(livro);
         }
 
-        public Livro? Get(int id)
+        public async Task<Livro?> GetAsync(int id)
         {
-            return LivrosClientRPC.Get(new IdLivro() { Id = id });
+            return await LivrosClientRPC.GetAsync(new IdLivro() { Id = id });
         }
 
-        public void Registrar(Livro livro)
+        public async Task RegistrarAsync(Livro livro)
         {
             _ = livro ?? throw new ArgumentNullException(nameof(livro));
             if (string.IsNullOrWhiteSpace(livro.Nome) || livro.Id < 0)
                 throw new InvalidDataException();
-            LivrosClientRPC.Registrar(livro);
+            await LivrosClientRPC.RegistrarAsync(livro);
         }
 
-        public void Remover(Livro livro)
+        public async Task RemoverAsync(Livro livro)
         {
             _ = livro ?? throw new ArgumentNullException(nameof(livro));
-            LivrosClientRPC.Remover(livro);
+            await LivrosClientRPC.RemoverAsync(livro);
         }
 
-        public IEnumerable<Livro> Buscar(string nome, string nomeAutor, IEnumerable<Tag>? tags)
+        public async IAsyncEnumerable<Livro> BuscarAsync(string nome, string nomeAutor, IEnumerable<Tag>? tags)
         {
             nome ??= "";
             nomeAutor ??= "";
             tags ??= new List<Tag>();
-            return LivrosClientRPC.Buscar(new ParamBusca() { NomeLivro = nome, NomeAutor = nomeAutor, Tags = { tags?.Select(t=> (RPC::Tag)t) } }).Livros.Select((l)=> (Livro)l!);
+			EnumLivros enumLivros = await LivrosClientRPC.BuscarAsync(new ParamBusca() { NomeLivro = nome, NomeAutor = nomeAutor, Tags = { tags?.Select(t => (RPC::Tag)t) } });
+			foreach (var livro in enumLivros.Livros)
+                yield return (Livro)livro!;
         }
-    }
+
+	}
 }
