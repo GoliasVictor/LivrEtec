3af6ca1c8cd6ba6d069e5ae1a3b3bf0ca21e7ee0
GoliasVictor <dev.jvsb@gmail.com>
2023-01-04 10:17:24 -0300

    Update ConfiguracaoTeste.cs
    Configuração estava falhando quando a URl da api não estava disponivel

--- src/LivrEtec.Testes/ConfiguracaoTeste.cs
+++ src/LivrEtec.Testes/ConfiguracaoTeste.cs
@@ -2,13 +2,13 @@ namespace LivrEtec.Testes;
 
 public sealed class ConfiguracaoTeste
 {
-	public ConfiguracaoTeste(string urlGIBAPI, string strConexaoMySQL)
+	public ConfiguracaoTeste(string strConexaoMySQL, string? urlGIBAPI = null)
 	{
 		UrlGIBAPI = urlGIBAPI;
 		StrConexaoMySQL = strConexaoMySQL;
 	}
 
-	public string UrlGIBAPI { get; set; }
+	public string? UrlGIBAPI { get; set; }
 	public string StrConexaoMySQL { get; set; }
 	
 }
