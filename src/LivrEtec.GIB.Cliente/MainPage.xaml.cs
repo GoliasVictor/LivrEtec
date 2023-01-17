2cad6e9c5be4a7fbda73ec4d658c4282fcadb1a9
GoliasVictor <Dev.jvsb@gmail.com>
2023-01-17 02:20:52 -0300

    criação cliente do gib

---
+++ src/LivrEtec.GIB.Cliente/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LivrEtec.GIB.Cliente
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
