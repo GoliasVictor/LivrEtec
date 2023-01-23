327cb54e1f7f13a97c1731485b919076f6e0eae3
GoliasVictor <dev.jvsb@gmail.com>
2023-01-23 13:27:34 -0300

    Adição serilog

--- src/LivrEtec.GIB.Servidor/Program.cs
+++ src/LivrEtec.GIB.Servidor/Program.cs
@@ -4,25 +4,43 @@ using LivrEtec.Servidor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.AspNetCore;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, services, configuration) =>{
+	Console.WriteLine("oi");
+	configuration
+		.ReadFrom.Configuration(context.Configuration)
+		.ReadFrom.Services(services)
+		.Enrich.FromLogContext()
+		.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:w3}] {SourceContext}: {Message}{NewLine}{Exception}")
+		.WriteTo.File(Path.Combine("./", "logs/txt/txt-.log"),
+			rollingInterval: RollingInterval.Day,
+			rollOnFileSizeLimit: true)
+		.WriteTo.File(new JsonFormatter(), "logs/json/json-.log",
+			rollingInterval: RollingInterval.Day,
+			rollOnFileSizeLimit: true);
+});
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 builder.Services.AddGrpc();
 
-
-builder.Logging.ClearProviders();
-builder.Logging.AddConsole();
 builder.Services.AddDbContextFactory<PacaContext>(( options )=>{
     var strConexao = builder.Configuration.GetConnectionString("MySql");
     options.UseMySql(strConexao, ServerVersion.AutoDetect(strConexao));
 });
 builder.Services.AddSingleton<IRelogio,RelogioSistema>(); 
 builder.Services.AddScoped<PacaContext>();
-builder.Services.AddLogging(configure => {
-    configure.AddConsole();
-}); 
+
 builder.Services.AddScoped<IRepUsuarios, RepUsuarios>();
 builder.Services.AddScoped<IRepAutores, RepAutores>();
 builder.Services.AddScoped<IRepLivros, RepLivros>();
@@ -39,5 +57,4 @@ var app = builder.Build();
 app.MapGrpcService<LivroServiceRPC>();
 app.MapGrpcService<EmprestimoServiceRPC>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-
 app.Run();
