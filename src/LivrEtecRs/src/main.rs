087284547e381483b6dd0e51907eed1c9d710625
GoliasVictor <dev.jvsb@gmail.com>
2023-05-24 03:56:37 -0300

    Criacao LivrEtec com Rust

---
+++ src/LivrEtecRs/src/main.rs
@@ -0,0 +1,5 @@
+pub mod models;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/LivrEtecRs/src/models.rs
@@ -0,0 +1,84 @@
+use datetime::Instant;
+pub type Id = i32;
+pub type Senha = String;
+pub enum Recurso<T> {
+	NaoCarregado(Id),
+	Carregado(T)
+}
+pub struct Pessoa {
+	pub id: Id,
+	pub nome : String, 
+	pub telefone: String, 
+	pub rm:  Option<String>
+}
+pub struct Autor {
+	pub id: Id, 
+	pub nome: String,
+}
+
+pub struct AutorLivro {
+	pub id_autor : Id,
+	pub id_livro : Id
+}
+pub struct Livro {
+	pub id : Id,
+    pub nome : String,
+    pub descricao : Option<String>,
+    pub arquivado : bool
+}
+
+pub struct TagLivro {
+	pub id_tag: Id,
+	pub id_livro : Id
+}
+
+pub struct Tag {
+	pub id : Id,
+	pub nome: String
+}
+pub struct Cargo {
+	pub id : Id,
+	pub nome: String,
+	pub permissoes : Vec<Permisao>
+}
+pub struct  Permisao {
+	pub id : Id,
+	pub nome: String,
+	pub descricao : String,
+	pub dependencias : Vec<Permisao>
+}
+pub struct  Usuario {
+	pub id : Id,
+	pub senha : Senha,
+	pub login : String, 
+	pub cargo : Recurso<Cargo>
+}
+
+pub struct Emprestimo {
+	pub id:Id,
+	pub livro : Recurso<Livro>,
+	pub pessoa : Recurso<Pessoa>,
+	pub usuario_criador : Recurso<Usuario>,
+	pub data_emprestimo: Instant,
+	pub fim_data_emprestimo: Instant,
+	pub estado : EstadosEmperstimo
+}
+pub enum EstadosEmperstimo {
+	Aberto,
+	Fechado {
+		data_fechamento : Instant,
+		usuario_fechador : Recurso<Usuario>,
+		devolvido : bool,
+		estado_devolucao :EstadoDevolucao,
+	}
+}
+pub enum EstadoDevolucao {
+	Devolvido  {
+		estado_atraso : EstadoAtraso
+	},
+	NaoDevolvido
+}
+pub enum EstadoAtraso {
+	Justificado(String),
+	NaoJustificado
+}
